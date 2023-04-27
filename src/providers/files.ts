5c435615a9fd94d4dc964bd0c15169c027b2f65b
ilya <ilya.frontd@gmail.com>
2023-04-27 23:15:00 +0200

    test cors

--- src/providers/files.ts
+++ src/providers/files.ts
@@ -2,7 +2,7 @@ export const getFiles = async () => {
   try {
     const res = await fetch('https://dev.test.sega.co.uk/api/list', {
       method: 'GET',
-      mode: 'no-cors',
+      mode: 'cors',
       headers: {
         'x-secret-api-key': 'Anwq2gS27c42znwPyzvY6xvUSvI8YOrj'
       }
